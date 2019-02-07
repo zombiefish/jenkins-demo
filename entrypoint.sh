aef8d29f59349cbd729c800f4ec33d8bc478fbdb
Andy Prowse <aprowse@highvail.com>
2019-02-07 18:35:54 +0000

    cleanup

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-openssl s_client -connect ${DTR_IP}:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee /usr/local/share/ca-certificates/${DTR_IP}.crt
-sudo update-ca-certificates
-
-/bin/tini -- /usr/local/bin/jenkins.sh
